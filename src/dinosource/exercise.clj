890d6aa23eccba625a02b5a02d1f7059553b6081
Chris Wilson <chris.wilson@hyves.nl>
2012-11-28 17:28:37 +0100

    Adding a better db

--- src/dinosource/exercise.clj
+++ src/dinosource/exercise.clj
@@ -1,12 +1,19 @@
 (ns dinosource.exercise)
 
+(def exercises
+  [{:title "Exercise 1"
+    :description "The first exercise"
+    :id 0
+    :library []
+    :script []}
+   {:title "Exercise 2"
+    :description "The second exercise"
+    :id 1
+    :library []
+    :script []}
+   ])
+
 (defn get-exercise
-  ([] {:exercises [{:title "Exercise 1"
-      	            :description "The first exercise"
-                    :id 0}
-                   {:title "Exercise 2"
-                  	:description "The second exercise"
-                  	:id 1}
-                   ]})
+  ([] {:exercises (map #(select-keys % [:title :description :id]) exercises)})
   ([ex]
-   (get (:exercises (dinosource.exercise/get-exercise)) ex)))
+   (filter #(= (:id %) ex) exercises)))
