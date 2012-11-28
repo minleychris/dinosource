636995e43912b821d9c86ba474d1dd0f7d647d41
Chris Wilson <chris.wilson@hyves.nl>
2012-11-28 16:38:02 +0100

    Adding in skeleton for posting the results of an exercise

--- src/dinosource/handler.clj
+++ src/dinosource/handler.clj
@@ -2,7 +2,8 @@
   	(:use compojure.core
           dinosource.exercise
           ring.util.response
-          [ring.middleware.format-response :only [wrap-restful-response]])
+          [ring.middleware.format-response :only [wrap-restful-response]]
+          [ring.middleware.format-params :only [wrap-restful-params]])
   	(:require	[compojure.handler :as handler]
     	    	[compojure.route :as route]
              	[clojure.data.json :as json]))
@@ -11,10 +12,13 @@
     (GET "/exercise" [] (response (get-exercise)))
     (GET "/exercise/:id" [id]
          (response (get-exercise (Integer. id))))
+    (POST "/exercise/:id" {params :params}
+         (response params))
     (route/files "/")
   	(route/not-found "Not Found"))
 
 (def app
 	(->
     	(handler/site app-routes)
+     	(wrap-restful-params)
     	(wrap-restful-response)))
