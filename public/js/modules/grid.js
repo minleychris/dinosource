fdbb5ff25e32931bdebc791825dd3c729dfdbe03
paco.gomezgranados <paco.gomezgranados@hyves.nl>
2012-11-29 11:33:33 +0100

    Grid creation

--- public/js/modules/grid.js
+++ public/js/modules/grid.js
@@ -5,7 +5,25 @@ function Grid(element, model) {
 
 Grid.prototype = {
 
+    ROWS: 10,
+    COLUMNS: 10,
+
     init: function(element, model){
 
+        this.element = element;
+        this.model = model;
+        this.createGrid();
+    },
+    
+    createGrid: function() {
+
+        for(var i=0; i<this.ROWS; i++) {
+            var newRow = $("<ul id='grid-row-'" + i + " class='grid-row'></ul>")
+            for(var j=0; j<this.COLUMNS; j++) {
+                var newCell = $("<li id='grid-cell-'" + i + "-" + j + " class='grid-cell'></li>")
+                newRow.append(newCell);
+            }
+            this.element.append(newRow);
+        }
     }
 };
