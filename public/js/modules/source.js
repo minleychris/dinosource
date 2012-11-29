6cb5b65943fefa52d321f5f937e0f76c99c8b8f2
paco.gomezgranados <paco.gomezgranados@hyves.nl>
2012-11-29 14:04:28 +0100

    Adding empty slots to source

--- public/js/modules/source.js
+++ public/js/modules/source.js
@@ -7,6 +7,18 @@ Source.prototype = {
 
     init: function(element, model){
 
+        this.element = element;
+        this.model = model;
+        this.prepareLayout();
+    },
+
+    prepareLayout: function() {
+
+        //If the first thing is not a slot container create it
+        if((this.element.children().length === 0) || !this.element.children().first().hasClass("js-source-slot") ) {
+
+            $("<div class='js-source-slot source-slot'>").prependTo(this.element);
+        }
     },
 
     load: function(data) {
